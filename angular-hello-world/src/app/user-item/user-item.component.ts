58d181ebd0d981f6b7fd1aea8d8ea3a7a2ab5918
EC2 Default User <ec2-user@ip-172-31-37-58.ec2.internal>
2019-02-06 12:54:47 +0000

    Array

--- angular-hello-world/src/app/user-item/user-item.component.ts
+++ angular-hello-world/src/app/user-item/user-item.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+
+import {
+  Component,
+  OnInit,
+  Input // <--- aggiungi questo
+ } from '@angular/core';
 
 @Component({
   selector: 'app-user-item',
   templateUrl: './user-item.component.html',
   styleUrls: ['./user-item.component.css']
 })
+
 export class UserItemComponent implements OnInit {
-  name : string;
-  constructor() { 
-    this.name = 'Alex';
+  @Input() name: string; // <-- Aggiungi la notazione @input
+
+  constructor() {
+     // Rimuovi l'impostazione del nome
   }
 
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
